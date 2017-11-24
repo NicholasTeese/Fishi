461a7146a68e276fda9263f888c6b31f77c5e8a9
Unknown <Mr.Teese@hotmail.com>
2017-11-24 12:54:24 +1100

    Script + prefab updates
    Added scrolling cloud script and updated cloud prefab to include script

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    private float m_fSpeed = 0;
+
+    public GameObject m_RightBounds;
+
+    void Awake()
+    {
+        m_fSpeed = Random.Range(0.01f, 0.08f);
+	}
+	
+	void Update()
+    {
+        transform.Translate(m_fSpeed, 0, 0);
+
+        if (transform.position.x > m_RightBounds.transform.position.x)
+        {
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        m_fSpeed = Random.Range(0.01f, 0.08f);
+        transform.position = new Vector2(-11, Random.Range(2.5f, 4.5f));
+    }
+}
